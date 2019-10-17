e8b6b06daefa534871550425c970f522d340ffc1
QuangMinh187pm13970 <quangminhkt2000@gmail.com>
2019-10-17 11:09:28 +0700

    Account

---
+++ Problem1/Problem1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("amount");
+            int money = int.Parse(Console.ReadLine());
+            int c, d;
+            Console.WriteLine("Withdraw");
+            int Withdraw = int.Parse(Console.ReadLine());
+            c = money - Withdraw;
+            Console.WriteLine("Balance " + money + "-" + Withdraw + "=" + c);
+            Console.WriteLine("amount " + c);
+            Console.WriteLine("Deposit");
+            int Deposit = int.Parse(Console.ReadLine());
+            d = c + Deposit;
+            Console.WriteLine("Balance " + c + "+" + Deposit + "=" + d);
+        }
+    }
+}
