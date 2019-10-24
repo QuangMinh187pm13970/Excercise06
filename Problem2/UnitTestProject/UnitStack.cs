2c0bc46798a6cf326c3e3854f2d30985c7a703a9
QuangMinh187pm13970 <quangminhkt2000@gmail.com>
2019-10-24 17:59:10 +0700

    Stack

---
+++ Problem2/UnitTestProject/UnitStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem2;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitStack
+    {   
+        [TestMethod]
+        public void TestMethodPush()
+        {
+            MyStack S = new MyStack();
+            S.Push("1");
+            S.Push("2");
+            S.Push("3");
+            Assert.AreEqual(3, S.Count());
+        }
+        [TestMethod]
+        public void TestMethodPop()
+        {
+            MyStack S = new MyStack();
+            S.Push("1");
+            S.Push("2");
+            S.Push("3");
+            Assert.AreEqual("3", S.Pop());
+            Assert.AreEqual("2", S.Pop());
+        }
+        [TestMethod]
+        public void TestMethodPeek()
+        {
+            MyStack S = new MyStack();
+            S.Push("1");
+            S.Push("2");
+            S.Push("3");
+            Assert.AreEqual("3", S.Peek());
+            Assert.AreEqual(3, S.Count());
+        }
+        [TestMethod]
+        public void TestMethodClear()
+        {
+            MyStack S = new MyStack();
+            S.Push("1");
+            S.Push("2");
+            S.Push("3");
+            S.Clear();
+            Assert.AreEqual(0,S.Count());
+        }
+    }
+}
